a0640bf8aa70a5905a3c80f7cdb28f3d34548d0e
hhakangull <hakanngul@icloud.com>
2023-06-13 20:54:37 +0100

    Initial Commit

---
+++ CalismaYapisi/CalismaYapisiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalismaYapisiApp.swift
+//  CalismaYapisi
+//
+//  Created by Hakan Gül on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalismaYapisiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalismaYapisi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CalismaYapisi
+//
+//  Created by Hakan Gül on 13/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
