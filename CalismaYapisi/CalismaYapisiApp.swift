485df17a0a6368eb5dbe8a60a25fcb48d1ff9d62
Hakan Gul <hakanngul@icloud.com>
2023-06-17 22:53:47 +0100

    navigation stack bar kullanımı tamamlandı.

--- CalismaYapisi/CalismaYapisiApp.swift
+++ CalismaYapisi/CalismaYapisiApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CalismaYapisiApp: App {
     var body: some Scene {
         WindowGroup {
-            Anasayfa()
+            //Anasayfa()
+            NavSayfa()
         }
     }
 }
--- CalismaYapisi/NavSayfa.swift
+++ CalismaYapisi/NavSayfa.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct NavSayfa: View {
+    
+    init(){
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .black
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
     var body: some View {
         NavigationStack {
             VStack {
                 Text("NavSayfa")
             
             }.navigationTitle("Sayfa")
-                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            print("Button Tapped")
+                        }) {
+                            Image(systemName: "arrowshape.turn.up.backward.fill")
+                        }.foregroundColor(.white)
+                    }
+                    
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button("Ayarlar") {
+                            print("Ayarlara Tıklandı")
+                        }
+                        .foregroundColor(.white)
+                        Button(action: {
+                            print("Profile Tıklandı")
+                        }) {
+                            Image(systemName: "person.fill")
+                        
+                        }
+                        .foregroundColor(.white)
+                        Button {
+                            print("Güç Kapatıldı")
+                        } label: {
+                            Image(systemName: "power")
+                        }.foregroundColor(.white)
+                    
+                    }
+                }
         }
     }
 }
