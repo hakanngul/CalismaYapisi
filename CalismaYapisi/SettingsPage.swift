8eadc674e8aa5353b8e0fb0720f5ddea870f74a0
Hakan Gul <hakanngul@icloud.com>
2023-06-17 23:04:43 +0100

    Tab View Kullanımı Tamamlandı

---
+++ CalismaYapisi/SettingsPage.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsPage.swift
+//  CalismaYapisi
+//
+//  Created by Hakan Gül on 17/06/2023.
+//
+
+import SwiftUI
+
+struct SettingsPage: View {
+    var body: some View {
+        Text("Settings View")
+            .font(.system(size: 50))
+    }
+}
+
+struct SettingsPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsPage()
+    }
+}
