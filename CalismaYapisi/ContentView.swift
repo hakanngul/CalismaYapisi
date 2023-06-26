e6ccaf5a837cbd24c034dbd7d8dc40f21ef3051c
Hakan Gul <hakanngul@icloud.com>
2023-06-26 23:05:00 +0100

    switch kullanımı

--- CalismaYapisi/ContentView.swift
+++ CalismaYapisi/ContentView.swift
@@ -8,28 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var resimAdi = "mutlu";
+    @State private var switchState = false
     var body: some View {
         VStack () {
             Spacer()
             
-            Image(resimAdi).resizable().frame(width: 200, height: 200, alignment: .center)
+            Toggle("Switch", isOn: $switchState)
+                .onChange(of:switchState){ value in
+                    print("Value of : \(value)")
+                }
+                .padding()
+                .toggleStyle(SwitchToggleStyle(tint: .red))
+            
+            //print switch state on Text and size also 20
+            Text(switchState ? "ON" : "OFF")
+                .font(.system(size: 20))
+                .padding()
             
             
-            Spacer()
-            
-            Button("Resim 1") {
-                resimAdi = "mutlu"
-            }
-            .font(.system(size: 30))
-            .padding()
-            .background(.red)
-            .foregroundColor(.white)
-            .cornerRadius(30)
-            .padding()
             
             Button("Resim 2") {
-                resimAdi = "uzgun"
+               
             }.font(.system(size: 30))
                 .padding()
                 .background(.blue)
