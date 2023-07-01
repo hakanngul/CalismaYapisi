e5155e6e73d42e02f1b4462f78cb44912420b575
hhakangull <hakanngul@icloud.com>
2023-07-01 18:48:05 +0100

    dinamik listeleme ve veri paylaşımı

--- CalismaYapisi/Listeleme/DinamikListeleme.swift
+++ CalismaYapisi/Listeleme/DinamikListeleme.swift
@@ -9,10 +9,25 @@ import SwiftUI
 
 struct DinamikListeleme: View {
     @State private var countryList = [Country]()
+    
+    func remove(at offsets: IndexSet) {
+        let ulke = countryList[offsets.first!]
+        
+        countryList.remove(at: offsets.first!)
+    }
+    
+    
     var body: some View {
         NavigationStack {
-            CountryListView(listOfCountry: countryList)
-            .navigationTitle("Ülkeler")
+            List {
+                ForEach(countryList, id: \.self.name) { country in
+                   NavigationLink(destination: CountryDetail(country: country)) {
+                    CountryListView(country: country)
+                    }
+                }
+                .onDelete(perform: remove)
+                .navigationTitle("Ülkeler")
+            }
         }.onAppear(){
             var list = [Country]()
             list.append(Country(name: "Türkiye", capital: "Ankara"))
@@ -20,20 +35,29 @@ struct DinamikListeleme: View {
             list.append(Country(name: "Fransa", capital: "Paris"))
             list.append(Country(name: "İngiltere", capital: "Londra"))
             list.append(Country(name: "İtalya", capital: "Roma"))
-            
             countryList = list
         }
     }
 }
 
+//how to send data to up struct
+
 struct CountryListView : View {
-    var listOfCountry = [Country]()
+    let country: Country
+    @State private var alert = false
     var body: some View {
-        List(listOfCountry, id: \.self.name) { country in
-            NavigationLink(destination: CountryDetail(country: country)) {
-                Text("\(country.name ?? "") - \(country.capital ?? "")")
-            }
-        }//.listStyle(PlainListStyle()) klasik liste görünümü
+        HStack {
+            Text(country.name ?? "")
+            Spacer()
+            Text("Seç")
+                .foregroundColor(.red)
+                .onTapGesture {
+                    alert.toggle()
+                }
+                .alert(isPresented: $alert) {Alert(title: Text("Seçilen Ülke"),message: Text(country.name!))
+                }
+            
+        }
     }
 }
 
